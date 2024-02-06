ca1cb5b8d87b688dbd8ee6b3444392fd118d89c0
Raghav Thakar <raghavthakar12@gmail.com>
2024-02-05 23:19:50 -0800

    file rename and param tweak

--- picarx/line_following_multithreaded.py
+++ picarx/line_following_multithreaded.py
@@ -17,8 +17,8 @@ if __name__ == "__main__":
     # controller.consume(pos_bus, 0.05)
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-        eSensor = executor.submit(sensor.produce, sensor_reading_bus, 0.1)
-        eInterpreter = executor.submit(interpreter.consume_produce, sensor_reading_bus, pos_bus, 0.1)
-        eController = executor.submit(controller.consume, pos_bus, 0.1)
+        eSensor = executor.submit(sensor.produce, sensor_reading_bus, 0.2)
+        eInterpreter = executor.submit(interpreter.consume_produce, sensor_reading_bus, pos_bus, 0.2)
+        eController = executor.submit(controller.consume, pos_bus, 0.2)
 
     eSensor.result()
