6b284c287e30b86d161ca6ed483edd2101ccc4ed
Raghav Thakar <raghavthakar12@gmail.com>
2024-02-05 23:15:01 -0800

    tweak multithread params

--- picarx/line_following multithreaded.py
+++ picarx/line_following multithreaded.py
@@ -16,9 +16,9 @@ if __name__ == "__main__":
     # interpreter.consume_produce(sensor_reading_bus, pos_bus, 0.1)
     # controller.consume(pos_bus, 0.05)
 
-    with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
-        eSensor = executor.submit(sensor.produce, sensor_reading_bus, 0.1)
-        eInterpreter = executor.submit(interpreter.consume_produce, sensor_reading_bus, pos_bus, 0.1)
-        eController = executor.submit(controller.consume, pos_bus, 0.05)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+        eSensor = executor.submit(sensor.produce, sensor_reading_bus, 0.01)
+        eInterpreter = executor.submit(interpreter.consume_produce, sensor_reading_bus, pos_bus, 0.01)
+        eController = executor.submit(controller.consume, pos_bus, 0.01)
 
     eSensor.result()
